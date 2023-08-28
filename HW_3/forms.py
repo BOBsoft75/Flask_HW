3c8881623a96bddba912f925dbc89c76e3a879a1
BOBsoft75 <bobsoft_spb@mail.ru>
2023-08-28 21:43:15 +0300

    HW_3

--- HW_3/forms.py
+++ HW_3/forms.py
@@ -1,16 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
 
 
-class LoginForm(FlaskForm):
-    name = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-
-class RegisterForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_pas = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
-    
+class RegistrationForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=8)])
--- HW_3/hw3.py
+++ HW_3/hw3.py
@@ -1,55 +1,50 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template, redirect, url_for
+from flask_wtf import CSRFProtect
+import secrets
+from werkzeug.security import generate_password_hash
+
+from forms import RegistrationForm
 from models import db, User
-from forms import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex()
+csrf = CSRFProtect(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db.init_app(app)
-# db = SQLAlchemy(app)
-
-
-category = [
-    {"title": 'Home page', "func_name": 'index'},
-]
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html', category=category)
-# def index():
-#     return 'Hi!'
+    return 'New Hello'
+
+
+@app.route('/new_register/', methods=['GET', 'POST'])
+def new_register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        surname = form.surname.data
+        email = form.email.data
+        password = form.password.data
+        hashed_password = generate_password_hash(password)
+        user = User(username=name, usersurname=surname,
+                    email=email, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('success', name=name))
+    return render_template('new_register.html', form=form)
+
+
+@app.route('/success/<name>')
+def success(name):
+    return render_template('success.html', name=name)
 
 
 @app.cli.command("init-db")
 def init_db():
     db.create_all()
     print('Ok')
-
-
-# @app.cli.command("add-john")
-# def add_user():
-#     user = User(username='john', usersurname='johnson', password='12345',
-#                 email='john@example.com')
-#     db.session.add(user)
-#     db.session.commit()
-#     print('John added to DB!')
-
-
-@app.route('/log/', methods=['GET', 'POST'])
-def log():
-    if request.method == 'POST':
-        session['username'] = request.form.get('username')
-        session['usersurname'] = request.form.get('usersurname')
-        session['email'] = request.form.get('email')
-        session['password'] = request.form.get('password')
-        user = User(username={session['username']}, usersurname={session['usersurname']},
-                    email={session['email']}, password={session['password']})
-    db.session.add(user)
-    db.session.commit()
-    print('User added to DB!')
-    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
